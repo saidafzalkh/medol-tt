1ff47fccf4d5849b0e3d33c99272f53e538e1ea0
Saidafzal Kholkhujaev <saidafzal1209@gmail.com>
2023-09-02 01:27:10 +0500

    slide

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Slide from "@/components/Slide";
 
 export default function Home() {
   return (
-    <main>
+    <main className="page-wrapper">
       <section className="hero">
         <Header />
+        <Slide />
       </section>
     </main>
   );
---
+++ components/Slide.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import Image from "next/image";
+import React, { useState } from "react";
+
+import slide1 from "@/public/slides/slide-1.png";
+import slide2 from "@/public/slides/slide-2.png";
+import slide3 from "@/public/slides/slide-3.png";
+
+const Slide: React.FC = () => {
+  const [activeSlide, setActiveSlide] = useState<number>(2);
+  const slides = [
+    {
+      id: 1,
+      title: (
+        <h2>
+          Аппарат ИК-лазерный <br /> МАКДЭЛ-09.1
+        </h2>
+      ),
+      content:
+        "Ориентированный на среднюю или высокую производительность тестов, анализатор ABL800 FLEX измеряет полный набор параметров, включая pH, газы крови, электролиты, метаболиты и показатели оксиметрии",
+      image: slide2,
+    },
+    {
+      id: 2,
+      title: (
+        <h2>
+          Анализатор <br /> ABL800 FLEX
+        </h2>
+      ),
+      content:
+        "Ориентированный на среднюю или высокую производительность тестов, анализатор ABL800 FLEX измеряет полный набор параметров, включая pH, газы крови, электролиты, метаболиты и показатели оксиметрии",
+      image: slide1,
+    },
+    {
+      id: 3,
+      title: (
+        <h2>
+          Система для <br /> аутогемотрансфузии
+        </h2>
+      ),
+      content:
+        "Ориентированный на среднюю или высокую производительность тестов, анализатор ABL800 FLEX измеряет полный набор параметров, включая pH, газы крови, электролиты, метаболиты и показатели оксиметрии",
+      image: slide3,
+    },
+  ];
+
+  const handleDotClick = (slideId: number) => {
+    setActiveSlide(slideId);
+  };
+
+  return (
+    <div className="container">
+      <div className="slide-container">
+        <div className="slides">
+          {slides.map((slide) => (
+            <div
+              key={slide.id}
+              className={`slide ${activeSlide === slide.id ? "active" : ""}`}
+            >
+              <div className="slide__content">
+                {slide.title}
+                <p>{slide.content}</p>
+              </div>
+              <div className="slide__image">
+                <Image src={slide.image} alt="Slider Image" />
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="dot-indicators">
+          {slides.map((slide) => (
+            <div
+              key={slide.id}
+              className={`dot ${activeSlide === slide.id ? "active" : ""}`}
+              onClick={() => handleDotClick(slide.id)}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
